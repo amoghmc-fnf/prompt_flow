e3c26381a12d989c6f1ece5a003c55da967d991a
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2025-02-28 12:11:46 +0530

    move system prompt to file

--- azure-open-ai-test.py
+++ azure-open-ai-test.py
@@ -4,9 +4,9 @@ from openai import AzureOpenAI
 
 
 
-def get_text(pdf_name: str) -> str:
+def get_text_from_file(txt_file: str) -> str:
     content = ""
-    with open(pdf_name, 'r') as file:
+    with open(txt_file, 'r') as file:
         # Read all the text from the file
         content = file.read()
     return content
@@ -41,27 +41,27 @@ def get_extracted_content():
         api_key=os.getenv("AZURE_OPENAI_API_KEY"),
         api_version="2024-02-01"
     )
-    # Example input text
-    input_text = get_text("extracted_content.txt")
+
+    input_text = get_text_from_file("extracted_content.txt")
+    system_prompt = get_text_from_file("system_prompt.txt")
 
     # Define the maximum number of tokens per chunk
     max_tokens = 100000  # Adjust based on your model's token limit
 
     # Chunk the input text
     chunks = chunk_text(input_text, max_tokens)
-    print(len(chunks))
+
     # Process each chunk
     ai_response = ""
     for chunk in chunks:
         response = client.chat.completions.create(
             model="gpt-4o-mini-std",  # model = "deployment_name".
             messages=[
-                {"role": "system", "content": "You are an extraction assistant that extracts titles from the content and returns a json response with key value pairs. You will be given a template that will be either empty or pre filled and you will fill/improve them as required with the latest updated content."},
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": "TEMPLATE:\n" + ai_response},
                 {"role": "user", "content": "CONTENT:\n" + chunk}
             ]
         )
         ai_response = response.choices[0].message.content
-        time.sleep(60)
     
     return ai_response
